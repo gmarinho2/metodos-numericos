f9ad634a45bb30c431f6aac936bbf9594a8fd37e
Gabriel Marinho <104592062+gmarinho2@users.noreply.github.com>
2024-03-27 13:44:58 -0300

    Update bissecao.c

--- bissecao.c
+++ bissecao.c
@@ -1,25 +1,29 @@
-double bissecao(double (*funcao_ptr)(double), double a, double b, double epsilon)
+double bissecao(double (*f)(double), double epsilon, double a, double b)
 {
-    double x0 = a;
-    double x1 = b;
+    //a convergência do método é assegurada, garantindo que o algoritmo permaneça dentro do intervalo inicial
+    //a cada iteração, o tamanho do intervalo é reduzido pela metade
+    //além disso, o método requer uma estimativa inicial precisa. o valor escolhido no primeiro passo influencia 
+    //diretamente na quantidade de iterações 
 
-    while(fabs(x0 - x1) > epsilon)
+    double erroAbsoluto, x0, x1;
+    x0 = a;
+    x1 = b;
+    int i = 0;
+    do 
     {
-        printf("O x atual é: %lf\n", x0);
-        x1 = (a + b)/2;
-
-        if(funcao_ptr(x1) * funcao_ptr(a)<0)
+        erroAbsoluto = fabs(x1 - x0);
+        x0 = x1;
+        x1 = (a + b) / 2;
+        if (f(x1) * f(a) < 0) 
         {
             b = x1;
-        }
-        else
+        } 
+        else 
         {
             a = x1;
         }
-        
-        x0 = x1;
-        printf("O x atual é: %lf\n", x0);
-    } 
+        printf("Iteração %d: x atual é: %lf\n", i = i+1, x0);
+    } while (erroAbsoluto > epsilon && f(x1) != 0);
 
     return x0;
 }
